f8feed9b6a6808381e05e8dffc872caf33f1e3a0
ceseduramirezj <ceseduramirezj493@gmail.com>
2024-03-19 09:29:58 +0100

    Otorgar acceso a front desde local

--- src/main/java/net/connectionchecker/springboot/controller/ConnectionRecordController.java
+++ src/main/java/net/connectionchecker/springboot/controller/ConnectionRecordController.java
@@ -11,10 +11,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/connection_record")
@@ -33,6 +30,7 @@ public class ConnectionRecordController {
         return new ResponseEntity<>(savedConnectionRecord, HttpStatus.CREATED);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/insertMany")
     public ResponseEntity<List<ConnectionRecordDto>> insertMany(@RequestBody List<ConnectionRecordDto> connectionRecords){
         List<ConnectionRecordDto> savedConnectionRecords = new ArrayList<>();
